7b6963e64e7275061029f484f538a9ade7ff8d14
Jiwoong Jang <jiwoong0218@gmail.com>
2021-09-02 06:28:48 +0900

    순위 검색 문제(미해결) 효율성 테스트 실패

--- search_rank.py
+++ search_rank.py
@@ -2,15 +2,14 @@ def solution(info, query):
     answer = []
     info_len = len(info)
     for i in range(info_len):
-        info[i] = list(map(str, info[i].split()))
+        info[i] = info[i].split()
     for i in range(len(query)):
-        query[i] = list(map(str, query[i].split()))
+        query[i] = query[i].split()
     for i in range(info_len):
         info[i][4] = int(info[i][4])
     info.sort(key=lambda x : x[4])
     for i in range(len(query)):
-        query[i] = [query[i][0], query[i][2], query[i][4], query[i][6], int(query[i][7])]
-        target = query[i][-1]
+        target = int(query[i][-1])
         left = 0
         right = info_len-1
         while left <= right:
@@ -22,7 +21,7 @@ def solution(info, query):
         count = 0
         for j in range(left, info_len):
             for k in range(4):
-                if not (query[i][k] == '-' or query[i][k] == info[j][k]):
+                if not (query[i][k*2] == '-' or query[i][k*2] == info[j][k]):
                     break
                 if k == 3:
                     count += 1
